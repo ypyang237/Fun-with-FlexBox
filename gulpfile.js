50e19ae2940eb9dfe7bdc97936d6e1ac2ddfcf08
Pamela Yang <PamelaYang@Pamelas-MacBook-Pro.local>
2016-03-26 20:48:51 -1000

    completes 1-4 challenge less the bonus one

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,24 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var connect = require('gulp-connect');
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
 
-gulp.task('connect', function(){
-  connect.server({
-    root: 'public',
-    livereload: true
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+  browserSync.init({
+      server: "./public"
   });
+
+  gulp.watch("scss/**/*.scss", ['sass']);
+  gulp.watch("public/*.html").on('change', browserSync.reload);
 });
 
-// keeps gulp from crashing for scss errors
-gulp.task('sass', function () {
-  return gulp.src('./scss/*.scss')
-      .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest('./public/css'));
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+  return gulp.src("scss/styles.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("public/css"))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('livereload', function (){
-  gulp.src('./public/**/*')
-  .pipe(connect.reload());
-});
-
-gulp.task('watch', function () {
-  gulp.watch('./scss/**/*.scss', ['sass']);
-  gulp.watch('./public/**/*', ['livereload']);
-});
-
-gulp.task('default', ['connect', 'watch', 'sass']);
+gulp.task('default', ['serve']);
